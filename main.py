2bcf16fd252849066d58db59df6196a0a8941672
Sean O'Neill <seanon3693@yahoo.com>
2021-05-02 21:57:07 +0100

    Visualization one complete

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import numpy as np
+from sqlalchemy import create_engine
+import pandas as pd
+import seaborn as sns
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+attrition = pd.read_csv('data/employee_attrition_train.csv')
+engine = create_engine('sqlite:///:memory:')
+attrition.to_sql('data_table', engine)
 
+# drop any duplicates in the data before any manipulation
+attrition.drop_duplicates()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Sorting OR indexing OR grouping
+# Replacing missing values OR dropping duplicates
+# Looping OR itterows
+# Merging dataframes
+# Use functions
+# Numpy
+# Dictionary OR list
+# 2 Visualizations
 
+# first visualization. Attrition X job satisfaction, grouped by daily rate
+# Remove any with empty daily rate
+visOne = attrition.dropna(subset=['Age'])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# sns.set_style("whitegrid")
+# g = sns.lmplot(x="Age", y="DailyRate", data=visOne, aspect=2)
+# g = (g.set_axis_labels("Age","Daily Rate").set(xlim=(18, 60), ylim=(0, 1500)))
+# plt.title("Daily Rate by Age")
+# plt.show()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Draw a nested barplot by species and sex
+g = sns.catplot(
+    data=visOne, kind="bar",
+    x="Department", y="MonthlyIncome", hue="Attrition",
+    ci="sd", palette="dark", alpha=.6, height=6
+)
+g.despine(left=True)
+g.set_axis_labels("", "Monthly Income")
+g.legend.set_title("")
+plt.show()
