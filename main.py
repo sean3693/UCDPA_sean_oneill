8b2e48779b379e6c1b1a623fdf3d030a7d3e2d62
Sean O'Neill <seanon3693@yahoo.com>
2021-05-02 23:39:18 +0100

    Progress on visualization two

--- main.py
+++ main.py
@@ -11,18 +11,9 @@ attrition.to_sql('data_table', engine)
 # drop any duplicates in the data before any manipulation
 attrition.drop_duplicates()
 
-# Sorting OR indexing OR grouping
-# Replacing missing values OR dropping duplicates
-# Looping OR itterows
-# Merging dataframes
-# Use functions
-# Numpy
-# Dictionary OR list
-# 2 Visualizations
-
 # first visualization. Attrition X job satisfaction, grouped by daily rate
 # Remove any with empty daily rate
-visOne = attrition.dropna(subset=['Age'])
+visOne = attrition.dropna(subset=['MonthlyIncome'])
 
 # sns.set_style("whitegrid")
 # g = sns.lmplot(x="Age", y="DailyRate", data=visOne, aspect=2)
@@ -31,12 +22,36 @@ visOne = attrition.dropna(subset=['Age'])
 # plt.show()
 
 # Draw a nested barplot by species and sex
-g = sns.catplot(
-    data=visOne, kind="bar",
-    x="Department", y="MonthlyIncome", hue="Attrition",
-    ci="sd", palette="dark", alpha=.6, height=6
-)
+g = sns.catplot(data=visOne, kind="bar", x="Department", y="MonthlyIncome", hue="Attrition", ci="sd", palette="dark", alpha=.6, height=6)
 g.despine(left=True)
 g.set_axis_labels("", "Monthly Income")
 g.legend.set_title("")
-plt.show()
+# plt.show()
+
+# Visualization 2
+# Average Satisfaction - Environment, Job, Relationship
+
+satisfaction = ["Low", "Medium" "High", "Very High"]
+
+# numpy array with all satisfaction columns from the dataset
+visTwo = attrition[['EnvironmentSatisfaction', 'JobSatisfaction', 'RelationshipSatisfaction']].to_numpy()
+
+# Sorting OR indexing OR grouping
+# Looping OR itterows
+# Merging dataframes
+# Use functions
+# 2 Visualizations
+
+# FIX ME - I should add each row in the 2d array
+def add_2_dimensional(data, index):
+    sum = 0
+    for val in data[index]:
+        sum = sum + val
+    return sum
+
+# FINISH ME - I should call the function to add the rows and then add them to a new array - this maybe should be a dataframe so we get the merging too
+sats = [len(visTwo)]
+for i in range(len(visTwo)):
+    sats.append(add_2_dimensional(visTwo, i))
+
+print(sats)
